895fd34e544343bc62ef8d0a0815e5b9fb5ace96
DieroKreator <qtechwired@gmail.com>
2024-10-15 00:09:10 -0300

    Created test skeleton

--- FormTest.cs
+++ FormTest.cs
@@ -1,12 +1,20 @@
-using OpenQA.Selenium;
-
 namespace FormTest;
+
+using System.Reflection;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools.V126.Page;
+
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
 
 [TestFixture]
 public class SendEnterInsuranceDataForm
 {
 
+#pragma warning disable NUnit1032 // An IDisposable field/property should be Disposed in a TearDown method
     private IWebDriver driver;
+#pragma warning restore NUnit1032 // An IDisposable field/property should be Disposed in a TearDown method
 
     [SetUp] // Configura um método para ser executado antes dos testes
     public void Before()
@@ -23,6 +31,49 @@ public class SendEnterInsuranceDataForm
     [Test] // Indica que é um método de teste
     public void SendForm()
     {
+        // driver.Navigate().GoToUrl("http://sampleapp.tricentis.com/101/app.php");
+
+        // Assert.That(bool.Parse(driver.Title), "Enter Vehicle Data");
+
+        driver = new ChromeDriver();
+
+        // Navigate to url and assert
+        var motorcycleInsurancePage = new MotorcycleInsurancePage(driver);
+        motorcycleInsurancePage.GoToUrl();
+        Assert.IsTrue(motorcycleInsurancePage.IsVisible);
+
+        // Fill out the form and submit
+        motorcycleInsurancePage.FillOutFormAndSubmit();
+        Assert.IsTrue(motorcycleInsurancePage.IsVisible);
 
     }
+
+    // private IWebDriver GetChromeDriver()
+    // {
+    //     var outPutDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+    //     return new ChromeDriver(outPutDirectory);
+    // }
+}
+
+internal class MotorcycleInsurancePage
+{
+    private IWebDriver driver;
+
+    public MotorcycleInsurancePage(IWebDriver driver)
+    {
+        this.driver = driver;
+    }
+
+    public bool? IsVisible { get; internal set; }
+
+    internal void FillOutFormAndSubmit()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal void GoToUrl()
+    {
+        throw new NotImplementedException();
+    }
+    
 }
